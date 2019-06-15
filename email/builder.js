a339e6a9411b74a4f36b1eaaf991afac88f72272
Bowser65 <contact.bowser65@gmail.com>
2019-06-15 22:23:52 +0200

    Add license header

--- email/builder.js
+++ email/builder.js
@@ -1,3 +1,21 @@
+/*
+ * Learndesk REST API
+ * Copyright (C) 2019 Learndesk
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 const mjml2html = require('mjml')
 const rimraf = require('rimraf')
 const path = require('path')
@@ -12,6 +30,7 @@ function buildFile (file) {
   const { html, errors } = mjml2html(mjml, {
     minify: true,
     filePath: file,
+    keepComments: false,
     validationLevel: 'strict'
   })
 

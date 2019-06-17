97979a073ce637018017e1c5624115f04d3a0ca3
Bowser65 <contact.bowser65@gmail.com>
2019-06-17 21:49:27 +0200

    this is why i need a linter

--- src/main/kotlin/app/learndesk/Mail.kt
+++ src/main/kotlin/app/learndesk/Mail.kt
@@ -22,11 +22,6 @@ import org.apache.commons.mail.HtmlEmail
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.text.DateFormat
-import java.time.ZoneId
-import java.time.format.DateTimeFormatter
-import java.time.format.FormatStyle
-import java.time.format.TextStyle
-import java.util.Calendar
 import java.util.Date
 import java.util.Locale
 import java.util.Properties
